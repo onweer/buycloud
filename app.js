207a3bf6d8ff6ac1f43e8750465fe9d415eb6a2a
hosn <xuhosn@gmail.com>
2016-09-28 11:13:57 +0800

    添加商品页面

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-app.listen(8000, function() {
+app.listen(8000, function () {
   console.log("listening at localhost port 8000");
 })
 
